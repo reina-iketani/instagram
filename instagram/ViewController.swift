48466cd0e6d78202a0fea3c2051dacf8ddef139a
reina-iketani <piyoko.reina.224@gmail.com>
2023-06-13 13:46:51 +0900

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Reina Iketani on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
